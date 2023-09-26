d5709f2f08e60e201825cb15dc5013795e6b4a5f
David Surumen <davidsurumen@gmail.com>
2023-09-26 16:36:09 +0300

    task 1 -babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Flask app module
+"""
+from flask import (
+    Flask,
+    render_template,
+)
+from flask_babel import Bable
+
+
+class Config:
+    """ Flask Babel configuration class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Bable(app)
+
+@app.route('/')
+def index():
+    """ Index route
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
