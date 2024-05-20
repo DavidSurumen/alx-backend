6c77855a139aa46ac9cbbd2b02fc67d621e7f8e7
David Surumen <davidsurumen@gmail.com>
2024-05-20 17:37:19 +0300

    task 2 -nodejs redis client basic operations. set and get

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,32 @@
+import { createClient } from 'redis';
+import redis from 'redis';
+
+const client = createClient();
+
+client.on('error', err => console.error('Redis client not connected to the server:', err));
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
+if (client.connected) {
+  client.quit();
+}
